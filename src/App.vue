018780cbb4b16afdf7140f3493fce7fd497cf13c
JoaoReis <joaovitor.olveira16@gmail.com>
2023-07-21 21:19:38 -0300

    finalização da aplicação, termino do sistema de busca

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
   <div id="app">
     <Navbar/>
     <div class="column is-half is-offset-one-quarter">
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <input type="text" name="" id="" placeholder="Buscar pokemon pelo nome" v-model="busca" class="input is-rounded ">
+      <button class="button is-fullwidth is-dark is-outlined" id="buscaBtn" @click="buscar">Pesquisar</button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
         <Pokemon :name="poke.name" :url="poke.url" :num="index+1" />
       </div>
     </div>
@@ -21,18 +23,40 @@ export default {
   name: 'App',
   data(){
     return{
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
       console.log("Pegou a lista de pokemons");
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
     })
   },
   components:{
     Pokemon,
     Navbar
+  },
+  methods:{
+    buscar: function(){
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+
+  },
+  computed:{
+    // resultadoBusca: function(){
+    //   if(this.busca == ''|| this.busca==' '){
+    //     return this.pokemons;
+    //   }else{
+    //     return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+    //   }
+    // }
   }
 
 }
@@ -45,5 +69,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+#buscaBtn{
+  margin-top: 2%;
 }
 </style>
